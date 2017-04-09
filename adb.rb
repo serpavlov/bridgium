9b9dcb9422c5518638c372c968e0cd75d5376ab3
Sergey Pavlov <Sergey.Pavlov@outlook.com>
2017-04-09 15:26:05 +0300

    formatting

--- adb.rb
+++ adb.rb
@@ -3,7 +3,6 @@ require 'open3'
 require 'rexml/document'
 
 class Adb
-  
   def adb(command)
     stdout, stderr, status = Open3.capture3("adb "+command)
     if !stderr.empty? && !stderr.include?('Success')
@@ -11,16 +10,16 @@ class Adb
     end
     return stdout
   end
-    
+
   def devices
     adb('devices').split("\n")[1..-1]
   end
-  
+
   def restart_adb
     adb 'kill-server'
     adb 'start-server'
   end
-  
+
   def install(path)
     if already_installed? Aapt.package_name(path)
       adb "install -r #{path}"
@@ -39,29 +38,29 @@ class Adb
       adb "uninstall #{package_name}"
     end
   end
-  
+
   def reinstall(path)
     uninstall(Aapt.package_name(path))
     install(path)
   end
-  
+
   def exec_command(command)
     adb "shell exec #{command}"
   end
-  
+
   def pull(source_path, out_path)
     adb "pull #{source_path} #{out_path}"
   end
-  
+
   def take_screenshot(path)
     exec_command 'screencap -p /sdcard/screenshot.png'
     pull '/sdcard/screenshot.png', path
   end
-  
+
   def take_screen_source
     exec_command 'uiautomator dump /sdcard/source.xml'
     pull '/sdcard/source.xml', 'temp/current_source.xml'
     source = File.read('temp/current_source.xml')
     view_hierarchy = REXML::Document.new(source)
-  end 
+  end
 end
