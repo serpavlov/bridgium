749774d88fcd1f426503ec08b30f0ae32f8bdd51
Sergey Pavlov <Sergey.Pavlov@kaspersky.com>
2017-04-12 17:16:12 +0300

    refactoring

--- server.rb
+++ server.rb
@@ -9,17 +9,17 @@ require_relative 'session'
 
 server = WEBrick::HTTPServer.new(:Port=>4328)
 
-sessions = Hash.new
-elements = Hash.new
+@sessions = Hash.new
+@elements = Hash.new
 
 server.mount_proc('/wd/hub/session'){ |req, resp|
   if req.request_method == 'POST'
     uuid = SecureRandom.uuid
-    sessions[uuid] = Hash.new
-    sessions[uuid][:session] = Session.new(uuid, nil)
+    @sessions[uuid] = Hash.new
+    @sessions[uuid][:session] = Session.new(uuid, nil)
 
     server.mount_proc("/wd/hub/session/#{uuid}/back"){ |req, resp|
-      sessions[uuid][:session].back
+      @sessions[uuid][:session].back
 
       resp_hash = { sessionId: uuid, status: 0 }
       resp['Content-Type'] = 'application/json'
@@ -28,72 +28,92 @@ server.mount_proc('/wd/hub/session'){ |req, resp|
     }
 
     server.mount_proc("/wd/hub/session/#{uuid}/source"){ |req, resp|
-      resp_hash = { sessionId: uuid, status: 0, value: sessions[uuid][:session].source }
+      resp_hash = { sessionId: uuid, status: 0, value: @sessions[uuid][:session].source }
       resp['Content-Type'] = 'application/json'
       resp.status = 200
       resp.body = JSON.generate(resp_hash)
     }
 
-    resp_hash ={ sessionId: uuid, value: new_session_hash, status: 0 }
+    resp_hash = { sessionId: uuid, value: new_session_hash, status: 0 }
     resp['Content-Type'] = 'application/json'
     resp.status = 200
     resp.body = JSON.generate(resp_hash)
 
-    server.mount_proc("/wd/hub/session/#{uuid}/element") do |req, resp|
-      found_element = sessions[uuid][:session].element(parse_locator req.body)
-
-      el_uuid = SecureRandom.uuid
-      elements[el_uuid] = found_element
-
-      server.mount_proc("/wd/hub/session/#{uuid}/element/#{el_uuid}/click") do |req, resp|
-        elements[el_uuid].click
-        resp_hash = { status: 0 }
-        resp['Content-Type'] = 'application/json'
-        resp.body = JSON.generate(resp_hash)
-        resp.status = 200
-      end
-
-      resp_hash = { sessionId: uuid, status: 0, value: { ELEMENT: el_uuid } }
-      resp['Content-Type'] = 'application/json'
-      resp.status = 200
-      resp.body = JSON.generate(resp_hash)
-    end
-
-    server.mount_proc("/wd/hub/session/#{uuid}/elements") do |req, resp|
-      found_elements = sessions[uuid][:session].elements(parse_locator req.body)
-
-      array_of_els_uuid = []
-      found_elements.each do |element|
-        el_uuid = SecureRandom.uuid
-        elements[el_uuid] = element
-        array_of_els_uuid.push el_uuid
-
-        server.mount_proc("/wd/hub/session/#{uuid}/element/#{el_uuid}/click") do |req, resp|
-          elements[el_uuid].click
-          resp_hash ={ status: 0 }
-          resp['Content-Type'] = 'application/json'
-          resp.body = JSON.generate(resp_hash)
-          resp.status = 200
-        end
-      end
-
-      resp_hash = { sessionId: uuid, value: [], status: 0 }
-
-      array_of_els_uuid.each do |element|
-        el_hash = { ELEMENT: element }
-        resp_hash[:value].push el_hash
-      end
-
-      resp['Content-Type'] = 'application/json'
-      resp.status = 200
-      resp.body = JSON.generate(resp_hash)
-    end
+    add_element(server, uuid, @sessions[uuid][:session])
+    add_elements(server, uuid, @sessions[uuid][:session])
   end
   #if req.request_method == 'DELETE'
   #  uuid = JSON.parse(req.body)
   #  server.umount("/session/#{uuid}")
   #end
 }
+
+def add_element(server, path, object)
+  server.mount_proc("/wd/hub/session/#{path}/element") do |req, resp|
+    found_element = object.element(parse_locator req.body)
+
+    el_uuid = SecureRandom.uuid
+    @elements[el_uuid] = found_element
+
+    add_element(server, "#{path}/element/#{el_uuid}", @elements[el_uuid])
+    add_elements(server, "#{path}/element/#{el_uuid}", @elements[el_uuid])
+    add_click(server, path.split('/').first, el_uuid)
+    add_text(server, path.split('/').first, el_uuid)
+
+    resp_hash = { sessionId: path.split('/').first, status: 0, value: { ELEMENT: el_uuid } }
+    resp['Content-Type'] = 'application/json'
+    resp.status = 200
+    resp.body = JSON.generate(resp_hash)
+  end
+end
+
+def add_elements(server, path, object)
+  server.mount_proc("/wd/hub/session/#{path}/elements") do |req, resp|
+    found_elements = object.elements(parse_locator req.body)
+
+    array_of_els_uuid = []
+    found_elements.each do |element|
+      el_uuid = SecureRandom.uuid
+      @elements[el_uuid] = element
+      array_of_els_uuid.push el_uuid
+
+      add_element(server, "#{path}/element/#{el_uuid}", @elements[el_uuid])
+      add_elements(server, "#{path}/element/#{el_uuid}", @elements[el_uuid])
+      add_click(server, path.split('/').first, el_uuid)
+      add_text(server, path.split('/').first, el_uuid)
+    end
+
+    resp_hash = { sessionId: path.split('/').first, value: [], status: 0 }
+
+    array_of_els_uuid.each do |element|
+      el_hash = { ELEMENT: element }
+      resp_hash[:value].push el_hash
+    end
+
+    resp['Content-Type'] = 'application/json'
+    resp.status = 200
+    resp.body = JSON.generate(resp_hash)
+  end
+end
+
+def add_click(server, session_uuid, el_uuid)
+  server.mount_proc("/wd/hub/session/#{session_uuid}/element/#{el_uuid}/click") do |req, resp|
+    @elements[el_uuid].click
+    resp_hash ={ status: 0 }
+    resp['Content-Type'] = 'application/json'
+    resp.body = JSON.generate(resp_hash)
+    resp.status = 200
+  end
+end
+
+def add_text(server, uuid, el_uuid)
+  server.mount_proc("/wd/hub/session/#{uuid}/element/#{el_uuid}/text") do |req, resp|
+    resp_hash ={ status: 0, value: @elements[el_uuid].text }
+    resp['Content-Type'] = 'application/json'
+    resp.body = JSON.generate(resp_hash)
+    resp.status = 200
+  end
+end
 
 def new_session_hash
     resp_hash = Hash.new
